37ab48a43292f010b40554e4a72f6c78a931ec47
MouseHappy123 <1351923644@qq.com>
2019-05-09 00:36:56 +0800

    I choose to die

---
+++ Backend/database/migrations/2019_05_08_155307_create_station_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStationTable extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('station1', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('passenger1', 10)->unique();
+            $table->string('passenger2', 10)->default('NULL');
+            $table->string('passenger3', 10)->default('NULL');
+            $table->string('destination', 20);
+            $table->text('comment');
+            $table->time('created_at');
+            $table->time('updated_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down()
+    {
+        Schema::dropIfExists('station');
+    }
+}
