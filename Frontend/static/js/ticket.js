cce729fe66fb9d317b5259d9c376c53c72ed5820
NancyLSH <1052567432@aq.com>
2019-05-11 01:56:03 +0800

    good night

--- Frontend/static/js/ticket.js
+++ Frontend/static/js/ticket.js
@@ -1,8 +1,15 @@
+const width = document.documentElement.clientWidth;
+const height = document.documentElement.clientHeight;
+
 $(function () {
     $("#update").click(function () {
-        window.location.href = "./update.php";
+        window.location.href = "update";
     })
     $("#return").click(function () {
-        window.location.href = "./portal.html";
+        window.location.href = "portal";
     })
+    var canvas = document.getElementById("canvas");
+    var ctx = canvas.getContext("2d");
+    canvas.height = height;
+    canvas.width = width;
 })
