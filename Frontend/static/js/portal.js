20117133a3b6e72c4ab34abf67dbe3d57bbe5afd
NancyLSH <1052567432@aq.com>
2019-05-11 18:56:57 +0800

    preload

--- Frontend/static/js/portal.js
+++ Frontend/static/js/portal.js
@@ -1,3 +1,7 @@
+var img = new Image();
+img.onload = function(){
+    img.src = "../pictures/1-1.png";
+}
 $(function () {
     $("button").click(function () {
         window.location.href = "../html/draw.html";
