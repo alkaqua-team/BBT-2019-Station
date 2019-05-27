7eccf010b017719e59892812ae915be88bb4888b
NancyLSH <1052567432@aq.com>
2019-05-27 16:25:00 +0800

    errbox

--- Frontend/static/js/portal.js
+++ Frontend/static/js/portal.js
@@ -16,19 +16,19 @@ $(function () {
     //调用微信分享接口
     wxshare();
 
-
-    // 活动时间检查 (此处先注释用于内测)
-    // var time = checkTime();
-    // if (time == 0) {
-    $("#entrance").click(function () {
-        window.location.href = "../html/draw.html";
-    });
-    // } else {
-    //     $(".err-box").show();
-    //     //禁用按钮
-    //     $("#entrance").attr("disabled", true);
-    // }
     $(".err-button").click(function () {
         $(".err-box").hide();
     })
+
+    活动时间检查 (此处先注释用于内测)
+    var time = checkTime();
+    if (time == 0) {
+    $("#entrance").click(function () {
+        window.location.href = "../html/draw.html";
+    });
+    } else {
+        $(".err-box").show();
+        //禁用按钮
+        $("#entrance").attr("disabled", true);
+    }
 });
